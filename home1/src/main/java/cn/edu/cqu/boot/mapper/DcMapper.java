f8421a66b9fe05f6701b09d25f22a6afecb60e19
Yxiangfeng <wang01234864@gmail.com>
2023-04-08 16:07:27 +0800

    数据表

---
+++ home1/src/main/java/cn/edu/cqu/boot/mapper/DcMapper.java
@@ -0,0 +1,12 @@
+/**
+ * @Classname DcMapper
+ * @Description TODO
+ * @Date 2023/4/8 15:04
+ * @Created by WangSanmu
+ */
+package cn.edu.cqu.boot.mapper;
+
+import com.github.yulichang.base.MPJBaseMapper;
+
+public interface DcMapper extends MPJBaseMapper {
+}
