9645898c7f0c60829418b146fc94ecdb3d485813
lxq <2396760025@qq.com>
2023-04-14 16:18:45 +0800

    设置病历访问拦截

--- home1/Vue/src/views/detailedMedicalRecord.vue
+++ home1/Vue/src/views/detailedMedicalRecord.vue
@@ -98,15 +98,22 @@ export default {
       serie1:[],
       ava:'',
       max:'',
+      user:''
     }
   },
 
   created() {
+    this.user = JSON.parse(sessionStorage.getItem("user"))||{}
     this.load()
   },
 
   methods: {
     load(){
+        console.log('病人名字',this.$route.query.patientName,'用户名字',this.user.name)
+      if(this.$route.query.doctorId == undefined || this.$route.query.patientName == undefined ||
+      this.$route.query.patientName !== this.user.name){
+        this.$router.push('/Info')
+      }
       request.get("/api/doctor/showCaseByDoctorIdPatientName",{
         params:{
           doctorId:this.$route.query.doctorId,
